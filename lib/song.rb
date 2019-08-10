b4a7986ac76b01c870282e3582a0a33285913123
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-10 20:16:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,11 +46,14 @@ class Song
     #parse data
     data = song.split(" - ")
     #binding.pry
-    attributes = data.collect do |x|
+    #attributes = data.collect do |x|
         artist_name = data[0]
         name = data[1]
-        Song.artist_name = artist_name
-        Song.name = name
+
+        new_song = self.new
+        new_song.artist_name = artist_name
+        new_song.name = name
+        new_song
     #set title and artist attributes
     #remove "mp3" from end
     #return a newly created song instance
