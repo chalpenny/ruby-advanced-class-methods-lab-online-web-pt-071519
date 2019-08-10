154389439db099ce94b06b1af9a6383f8af28e9f
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-10 20:41:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -59,8 +59,17 @@ class Song
   end
 
   def Song.create_from_filename(song)
-    song.new_from_filename
+    data = song.split(" - ")
+        artist_name = data[0]
+        name = data[1]
+        new_song = self.new
+        new_song.artist_name = artist_name
+        new_song.name = name.gsub(".mp3", "")
         @@all << new_song
   end
 
+  def self.destroy_all
+    @@all.clear
+  end
+  
 end
